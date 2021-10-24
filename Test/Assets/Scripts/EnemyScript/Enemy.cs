92c75c55aba1a5bac69bb0cc3d63d5ca9e6586ba
Alexey <ottens.a99@gmail.com>
2021-10-24 14:24:09 +0300

    -

---
+++ Test/Assets/Scripts/EnemyScript/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy :MonoBehaviour
+{
+    [SerializeField] protected int _hpCurrent;
+
+    private GameManager _gameManager;
+
+    //Эффект от получения урона
+    [SerializeField] SpriteRenderer _sprite;
+    private Color _damdgeMarker= new Color(1,.9f,.9f,1);
+    private Color _addedTone = new Color(0, 0.1f, 0.1f, 0);
+    private float _toneStep = 0.1f;
+
+    private Camera _cam;
+      
+
+    private void Start()
+    {
+        _gameManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+        _cam = Camera.main;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray _ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit _hit;
+            if (Physics.Raycast(_ray, out _hit, 100))
+            {
+
+                if (_hit.collider.TryGetComponent(out EnemyMelee _enemy))
+                {
+                    _enemy.GetDamage();
+                }
+                else if (_hit.collider.TryGetComponent(out EnemyAgiled _enemyAgilled))
+                {
+                    _enemyAgilled.GetDamage();
+                }
+
+
+            }
+
+        }
+    }
+
+    public virtual void GetDamage()
+    {
+        _hpCurrent--;
+        _sprite.color = _damdgeMarker - _addedTone;
+        _addedTone = new Color(0 , 0.1f+_toneStep, 0.1f+ _toneStep, 0);
+        _toneStep += 0.1f;
+        
+        if (_hpCurrent <= 0)
+        {
+            _gameManager.StartCoroutine("EnemySpawn");
+            _gameManager.AddScore();
+            DestroyImmediate(this.gameObject);           
+        }
+    }
+}
---
+++ Test/Assets/Scripts/EnemyScript/EnemyAgiled.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAgiled : Enemy
+{
+    [SerializeField] GameObject _blinkEffect;
+
+    public override void GetDamage()
+    {
+       
+        //Шанс уклониться от атаки
+        int _dodgeChance = 0;
+        int rand = Random.Range(0, 3);
+        if (_dodgeChance == rand)
+        {
+            transform.position = new Vector2(Random.Range(-2.5f, 2.5f), Random.Range(3f, -3f));
+            Instantiate(_blinkEffect, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            base.GetDamage();
+        }
+
+    }
+
+}
+
+    
+           
